3fe215887d85c6e5601f8a64f7a71e5d7c76347c
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-08-01 00:37:52 -0700

    Implemented N Queens problem solution.

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,74 +5,61 @@ Solve N Queens problem
 import sys
 
 
-def is_safe(board, row, col):
-    """
-    Check if (row, col) is safe
-    """
-    # Check this row on left side
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    return True
+class NQueen:
+    """ Class for solving N Queen """
 
+    def __init__(self, n):
+        """ Initialize variables """
+        self.n = n
+        self.board = [0 for _ in range(n)]
+        self.solutions = []
 
-def solve_n_queens(board, col, solutions):
-    """
-    Solve N Queens with backtracking
-    """
-    if col >= len(board):
-        solution = []
-        for i in range(len(board)):
-            for j in range(len(board)):
-                if board[i][j] == 1:
-                    solution.append([i, j])
-        solutions.append(solution)
+    def is_safe(self, row, col):
+        """ Check if (row, col) safe """
+        for i in range(row):
+            if self.board[i] == col or \
+               abs(self.board[i] - col) == row - i:
+                return False
         return True
-    res = False
-    for i in range(len(board)):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            res = solve_n_queens(board, col + 1, solutions) or res
-            board[i][col] = 0
-    return res
 
+    def solve(self, row):
+        """ Place queens recursively """
+        if row == self.n:
+            solution = [[i, self.board[i]] for i in range(self.n)]
+            self.solutions.append(solution)
+        else:
+            for col in range(self.n):
+                if self.is_safe(row, col):
+                    self.board[row] = col
+                    self.solve(row + 1)
 
-def print_solutions(solutions):
-    """
-    Print all solutions
-    """
-    for solution in solutions:
-        print(solution)
+    def get_solutions(self):
+        """ Get all solutions """
+        self.solve(0)
+        return self.solutions
 
 
 def main():
-    """
-    Main function to parse arguments and solve
-    """
+    """ Main function to solve """
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
+
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
+
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    solutions = []
-    solve_n_queens(board, 0, solutions)
-    print
-    _solutions(solutions)
+
+    queen = NQueen(n)
+    solutions = queen.get_solutions()
+
+    for solution in solutions:
+        print(solution)
 
 
 if __name__ == "__main__":
