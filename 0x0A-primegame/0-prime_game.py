8b93e32d5f30de81b913f65c898f53d79f4b1644
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-09-03 12:10:08 -0700

    Implement Prime Game winner determination.

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,56 +1,48 @@
 #!/usr/bin/python3
 
-def sieve_of_eratosthenes(max_n):
-    """Generate a list of primes up to max_n
-    using the Sieve of Eratosthenes."""
-    is_prime = [True] * (max_n + 1)
-    p = 2
-    while p * p <= max_n:
-        if is_prime[p]:
-            for i in range(p * p, max_n + 1, p):
-                is_prime[i] = False
-        p += 1
-    return [p for p in range(2, max_n + 1) if is_prime[p]]
+"""Prime Game"""
+
+
+def check_prime(n):
+    """Determine if n is a prime number."""
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
+            return False
+    return True
+
+
+def add_prime(n, primes):
+    """Populate the primes list up to n."""
+    last_prime = primes[-1]
+    if n > last_prime:
+        for i in range(last_prime + 1, n + 1):
+            if check_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
 
 
 def isWinner(x, nums):
-    """Determine the player who wins the most rounds."""
-    if not nums:
-        return None
+    """Determine who wins the most rounds of the game.
+    x: number of rounds
+    nums: list of n values for each round
+    Returns the name of the player who won the most rounds.
+    Returns None if the winner cannot be determined.
+    """
+    score = {"Maria": 0, "Ben": 0}
+    primes = [0, 0, 2]
+    add_prime(max(nums), primes)
 
-    max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
+    for round in range(x):
+        _sum = sum((i != 0 and i <= nums[round])
+                   for i in primes[:nums[round] + 1])
+        winner = "Maria" if _sum % 2 else "Ben"
+        if winner:
+            score[winner] += 1
 
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if n < 2:
-            ben_wins += 1
-            continue
-
-        # Initialize game state
-        numbers = [True] * (n + 1)
-        turn = 0  # 0 for Maria, 1 for Ben
-        for prime in primes:
-            if prime > n:
-                break
-            if numbers[prime]:
-                # Remove prime and its multiples
-                for multiple in range(prime, n + 1, prime):
-                    numbers[multiple] = False
-                # Switch turn
-                turn = 1 - turn
-
-        # Determine the winner of this round
-        if turn == 0:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
+    if score["Maria"] > score["Ben"]:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif score["Ben"] > score["Maria"]:
         return "Ben"
-    else:
-        return None
+
+    return None
